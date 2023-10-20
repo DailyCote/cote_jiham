6446f8d1acec9e0bdb5ba7b4de6e1ab520f8c824
JiHamPark <74188144+itzwe@users.noreply.github.com>
2023-10-20 14:30:10 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -2,8 +2,9 @@ function solution(a, b, n) {
     let result = 0;
     let now = 0;
 
+    // n : 현재 가지고 있는 빈병 수
     while (n >= a) {
-        now = parseInt(n / a) * b
+        now = parseInt(n / a) *b
         result += now
         n = now + n % a
     }
