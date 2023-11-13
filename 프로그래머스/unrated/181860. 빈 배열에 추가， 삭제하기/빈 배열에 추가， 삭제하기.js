03e9771d226c0eb4e98b70efee5ce1c923ebf071
JiHamPark <74188144+itzwe@users.noreply.github.com>
2023-11-13 14:34:32 +0900

    [level unrated] Title: 빈 배열에 추가, 삭제하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
@@ -0,0 +1,11 @@
+function solution(arr, flag) {
+    let answer = '';
+    for(let i=0; i<flag.length; i++){
+        if(flag[i]){
+            answer += String(arr[i]).repeat(arr[i]*2)
+        }else{
+            answer = answer.slice(0,answer.length-arr[i])
+        }
+    }
+    return answer.split('').map(i=>Number(i));
+}
