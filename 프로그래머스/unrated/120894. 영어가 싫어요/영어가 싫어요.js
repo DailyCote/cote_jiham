75d185f6ee0f36f9b49e8f19e2c079fb1feab2bb
JiHamPark <74188144+itzwe@users.noreply.github.com>
2023-12-13 14:27:20 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    let name = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+    let num =  [0,1,2,3,4,5,6,7,8,9];
+    for(let i=0; i<num.length; i++){
+        n = n.replaceAll(name[i], num[i]);
+    }
+    return +n
+}
